7c58e293257fb391d8e60674b1468b74f18a8040
Suz Topp <76724894+suztopp@users.noreply.github.com>
2023-08-18 15:23:08 +1000

    working - added in basic application properties settings for h2 and sql queries, and added the Customer model with basic class values etc - suz

---
+++ src/main/java/com/example/CustomerApplication/model/Customer.java
@@ -0,0 +1,64 @@
+package com.example.CustomerApplication.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Customers")
+public class Customer {
+
+    @Id
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate birthDate;
+
+    public Customer(String firstName, String lastName, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public Customer() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+}
